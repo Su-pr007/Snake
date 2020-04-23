2eeee23db13d3a31a78c572d2057beb6fb39d1cf
Su-pr007 <maksimka.suvorov.01@list.ru>
2020-04-23 12:45:13 +0300

    lesson 6

--- Program.cs
+++ Program.cs
@@ -12,27 +12,37 @@ namespace Snake
         {
 
             Point p1 = new Point(1, 3, '*');
-            /*p1.x = 1;
-            p1.y = 3;
-            p1.sym = '*';*/
             p1.Draw();
 
-            /*int x1 = 1;
-            int y1 = 3;
-            char sym1 = '*';
-            Draw(x1, y1, sym1);*/
-
-
             Point p2 = new Point(4, 5, '#');
-            /*p2.x = 4;
-            p2.y = 5;
-            p2.sym = '#';*/
             p2.Draw();
 
-            /*int x2 = 4;
-            int y2 = 5;
-            char sym2 = '#';
-            Draw(x2, y2, sym2);*/
+
+
+            List<int> numList = new List<int>();
+            numList.Add(0);
+            numList.Add(1);
+            numList.Add(2);
+
+            foreach (int i in numList)
+            {
+                Console.WriteLine(i);
+            }
+            numList.RemoveAt(0);
+
+            List<Point> pointList = new List<Point>();
+            pointList.Add(p1);
+            pointList.Add(p2);
+
+            List<char> sym1 = new List<char>();
+            sym1.Add('*');
+            sym1.Add('#');
+
+
+
+
+
+
 
             Console.ReadLine();
         }
