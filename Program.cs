5ff83782444674c6f6047bea44a74fcee5500d08
Su-pr007 <maksimka.suvorov.01@list.ru>
2020-04-25 13:53:42 +0300

    fixed lesson 7

--- Program.cs
+++ Program.cs
@@ -18,15 +18,13 @@ namespace Snake
             p2.Draw();
 
             HorizontalLine line1 = new HorizontalLine(0, 50, 0, '#');
-            line1.Draw();
-
             HorizontalLine line2 = new HorizontalLine(0, 50, 25, '#');
-            line2.Draw();
-
             VerticalLine line3 = new VerticalLine(0, 25, 0, '#');
-            line3.Draw();
-
             VerticalLine line4 = new VerticalLine(0, 25, 50, '#');
+
+            line1.Draw();
+            line2.Draw();
+            line3.Draw();
             line4.Draw();
 
             Console.ReadLine();
